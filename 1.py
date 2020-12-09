be78b778ee637f8e6a6139ae77fab5ec963b9fac
DoughnutJam <71940279+DoughnutJam@users.noreply.github.com>
2020-12-08 20:02:38 -0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,9 @@
+import sys; print(sys.executable)
+C:\Users\habib\AppData\Local\Programs\Python\Python38-32> python -m pip install numpy
+
+
 input=open("Input.txt","r")
-grid=list(input)
+
+
+grid = np.loadtxt("Input.txt", deliminter=",", dtype=string, skiprows=1)
 print(grid)

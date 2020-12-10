42ee1403d43383ff730ac2a5fb1375be693c80ae
DoughnutJam <71940279+DoughnutJam@users.noreply.github.com>
2020-12-09 19:24:07 -0700

    Update 1.py
    Added no tree counter to see if it was running to the bottom.  No dice.
    
    Modulo added as well to len of first array.
    
    Still not running to bottom.  Missing something.

--- 1.py
+++ 1.py
@@ -3,19 +3,22 @@
 input=open("Input.txt","r")
 lines=input.readlines()
 #get rid of /n
-print (lines[1][2])
-
+print (lines[0][3])
 
 x=0
 y=0
 
 treecounter=0
-
+notreecounter=0
 for line in lines:
-   x=+1
    y=+3 % (len(lines[0])-1)
+   #31 is len. modulo working. 
+   x=+1
    #use modulo operator to get a loop in y
    if lines[x][y] == '#':
       treecounter=+1
-
+   else:
+      notreecounter=+ 1
+      
 print(treecounter)
+print(notreecounter)

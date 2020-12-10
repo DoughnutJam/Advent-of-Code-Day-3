58ba6c53b748ee5a653a737fb751791046ea40e9
DoughnutJam <71940279+DoughnutJam@users.noreply.github.com>
2020-12-09 20:46:46 -0700

    Update 1.py
    Solved it,
    keep getting an extra tree.
    
    It's either above 1 or below 1. I must be counting an extra line somewhere.

--- 1.py
+++ 1.py
@@ -3,7 +3,6 @@
 input=open("Input.txt","r")
 lines=input.readlines()
 #get rid of /n
-print (lines[0][3])
 
 x=0
 y=0
@@ -11,15 +10,24 @@ y=0
 treecounter=0
 notreecounter=0
 for line in lines:
-   y=+3 % (len(lines[0])-1)
+   y=(y+3) % (len(lines[0])-1)
    #31 is len. modulo working. 
-   x=+1
-   #use modulo operator to get a loop in y
+   x=(x+1) % (len(lines))
+   #use modulo operator to get a circle in x & y
+
    if lines[x][y] == '#':
-      treecounter=+1
+      treecounter+=1
    else:
-      notreecounter=+ 1
+      notreecounter+=1
       
+   
 print(treecounter)
 print(notreecounter)
 
+#answer is 230
+
+103
+83
+98
+98
+print(103*83*98*98)

97259fafff08c9fd3c3663272e463a9cb34bb440
DoughnutJam <71940279+DoughnutJam@users.noreply.github.com>
2020-12-12 17:10:21 -0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,6 +2,7 @@
 
 input=open("Input.txt","r")
 lines=input.readlines()
+print(len(lines))
 #get rid of /n
 
 x=0
@@ -10,24 +11,18 @@ y=0
 treecounter=0
 notreecounter=0
 for line in lines:
-   y=(y+3) % (len(lines[0])-1)
-   #31 is len. modulo working. 
-   x=(x+1) % (len(lines))
+      if lines[x][y] == '#':
+         treecounter+=1
+      else:
+         notreecounter+=1
+      y=(y+3) % (len(lines[0])-1)
+   #31 is len. modulo working.
+      x=(x+1) % (len(lines))
    #use modulo operator to get a circle in x & y
 
-   if lines[x][y] == '#':
-      treecounter+=1
-   else:
-      notreecounter+=1
-      
-   
 print(treecounter)
 print(notreecounter)
 
 #answer is 230
 
-103
-83
-98
-98
-print(103*83*98*98)
+print(230*104*83)
